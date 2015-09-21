f0a9a14a22aab49d8a2ef491c6dd66391514d4fc
allenwhale <allenwhale@yahoo.com.tw>
2015-09-21 10:47:34 +0800

    udpate

--- icpc/icpc7150.cpp
+++ icpc/icpc7150.cpp
@@ -4,7 +4,7 @@ double s[1000010];
 int main(){
     double p,a,b,c,d;
     int n;
-    while(scanf("%lf%lf%lf%lf%lf%d",&p,&a,&b,&c,&d,&n)){
+    while(~scanf("%lf%lf%lf%lf%lf%d",&p,&a,&b,&c,&d,&n)){
         s[0]=0;
         for(int i=1;i<=n;i++){
             s[i]=p*(sin(a*i+b)+cos(c*i+d)+2);
