67e40e2bd998a1b3ec604539033eec74c175ee74
allenwhale <allenwhale@yahoo.com.tw>
2015-09-24 13:59:27 +0800

    update

---
+++ cf/557b.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+double c[100010];
+int main(){
+    int N;
+    double W;
+    scanf("%d %lf",&N,&W);
+    for(int i=0;i<N*2;i++){
+        scanf("%lf",&c[i]);
+    }
+    sort(c,c+2*N);
+    printf("%lf\n", min(min(W,c[0]*3*(double)N),c[N]/2.0*3.0*(double)N));
+    return 0;
+}
+
