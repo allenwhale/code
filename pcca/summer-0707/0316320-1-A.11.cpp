cf11b60fe166c224a6b2c63ad7a3f46196a93787
allenwhale <allenwhale@yahoo.com.tw>
2015-09-26 12:08:57 +0800

    update-v8

---
+++ pcca/summer-0707/0316320-1-A.11.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+double s[1000010];
+int main(){
+    double p,a,b,c,d;
+    int n;
+    while(~scanf("%lf%lf%lf%lf%lf%d",&p,&a,&b,&c,&d,&n)){
+        s[0]=0;
+        for(int i=1;i<=n;i++){
+            s[i]=p*(sin(a*i+b)+cos(c*i+d)+2);
+        }
+        int h=0;
+        double ans=0.0;
+        for(int i=1;i<=n;i++){
+            if(s[i]>s[h])h=i;
+            else ans=max(ans, s[h]-s[i]);
+        }
+        printf("%.7f\n", ans);
+    }
+    return 0;
+}
