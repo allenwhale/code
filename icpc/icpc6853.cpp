e700ee29a707ed3d67b9eca8e157ebf57b74de37
allenwhale <allenwhale@yahoo.com.tw>
2015-10-03 10:40:10 +0800

    update

--- icpc/icpc6853.cpp
+++ icpc/icpc6853.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
+#define INF 10000000
 using namespace std;
 int w[110][60], c[110][110];
 int dp[110][60];
 int S,C;
 int DP(int x,int n){
-    if(dp[x][n]!=-1)return dp[x][n];
-    dp[x][n]=-10000000;
+    if(dp[x][n]!=-INF)return dp[x][n];
     for(int i=0;i<S;i++){
         dp[x][n]=max(dp[x][n],DP(i,n-1)+w[x][C-n-1]-c[i][x]);
     }
@@ -22,7 +22,9 @@ int main(){
         for(int i=0;i<S;i++)
             for(int j=0;j<S;j++)
                 scanf("%d",&c[i][j]);
-        memset(dp,-1,sizeof(dp));
+        for(int i=0;i<S;i++)
+            for(int j=0;j<C;j++)
+                dp[i][j]=-INF;
         for(int i=0;i<S;i++)
             dp[i][0]=w[i][C-1];
         int ans=0;
