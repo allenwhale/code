e005624424376e4b05da176b7607435cdf04b596
allenwhale <allenwhale@yahoo.com.tw>
2015-10-03 11:02:15 +0800

    update

--- icpc/icpc6847.cpp
+++ icpc/icpc6847.cpp
@@ -4,6 +4,9 @@ typedef long long ll;
 int main(){
     ll a,b;
     while(~scanf("%lld%lld",&a,&b)&&a&&b){
+        bool aa=false,bb=false;
+        while(a%5ll!=0)aa=true,a--;
+        while(b%5ll!=0)bb=true,b--;
         printf("%lld\n",(b-a+1)/5ll+1ll);
     }
     return 0;
