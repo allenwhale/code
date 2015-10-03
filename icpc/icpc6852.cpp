85b6c6f17093b421352364d8dc3f64bba16c4f47
allenwhale <allenwhale@yahoo.com.tw>
2015-10-03 13:46:26 +0800

    update

--- icpc/icpc6852.cpp
+++ icpc/icpc6852.cpp
@@ -6,7 +6,7 @@ int lcm(int a,int b){
 int cnt[720720+10],a[100010],b[100010];
 int ans[100010];
 typedef pair<int,int> PI;
-map<PI,int>mp;
+int mp[3000];
 
 int main(){
     int T;
@@ -14,19 +14,20 @@ int main(){
     while(T--){
         int N,M;
         scanf("%d%d",&N,&M);
-        mp.clear();
         int total=1;
         for(int i=0;i<N;i++){
             scanf("%d%d",&a[i],&b[i]);
-            mp[PI(a[i],b[i])]+=1;
+            mp[a[i]*17+b[i]]++;
             total=lcm(total,b[i]);
         }
-        for(auto m: mp){
-            PI p=m.first;
-            int n=m.second;
-            for(int i=0;i<total;i++)
-                if(i%p.second<p.first)
-                    cnt[i]+=n;
+        for(int i=1;i<=16;i++){
+            for(int j=i;j<=16;j++){
+                int h=i*17+j;
+                if(mp[h])for(int k=0;k<total;k++){
+                    if(k%j<i)
+                        cnt[k]+=mp[h];
+                }
+            }
         }
         int r=M%total,d=M/total;
         for(int i=0;i<M&&i<total;i++){
