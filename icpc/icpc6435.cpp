4a68a37d785db75666bf27036648a462bd202b9f
allenwhale <allenwhale@yahoo.com.tw>
2015-10-28 22:50:49 +0800

    update

--- icpc/icpc6435.cpp
+++ icpc/icpc6435.cpp
@@ -4,12 +4,13 @@ using namespace std;
 typedef long long ll;
 ll dp[50010][210];
 int N,M,D;
-vector<int>A(N+1),B(M+1);
+vector<int>A,B;
 int dfs(int x,int y){
 	while(x<N&&B[y]-A[x]>D)x++;
 	while(y<M&&A[x]-B[y]>D)y++;
 	ll &res=dp[x][A[x]-B[y]+100];
-	if(x==N||y==M)return res=0;
+	if(x==N||y==M)return res=1;
+	if(res!=-1)return res;
 	res=0;
 	if(A[x]+D>B[y])res+=dfs(x,y+1);
 	if(B[y]+D>A[x])res+=dfs(x+1,y);
@@ -17,6 +18,8 @@ int dfs(int x,int y){
 }
 int Solve(){
 	scanf("%d%d%d",&N,&M,&D);
+	A=B=vector<int>(max(N,M)+1);
+	memset(dp,-1,sizeof(dp));
 	for(int i=0;i<N;i++)
 		scanf("%d",&A[i]);
 	for(int i=0;i<M;i++)
