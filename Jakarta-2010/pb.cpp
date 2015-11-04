a227e03af262b177f3f78567fd16de70a4345a24
allenwhale <allenwhale@yahoo.com.tw>
2015-11-04 19:23:14 +0800

    update

--- Jakarta-2010/pb.cpp
+++ Jakarta-2010/pb.cpp
@@ -9,21 +9,19 @@ bool Solve(){
     x2+=(1<<(N-1))-1;
     int up1=(y1+1)/2,down1=y1-up1;
     int up2=(y2+1)/2,down2=y2-up2;
-    int o1=y1/2!=up1,o2=y2/2!=up2;
     x1>>=up1;x1<<=down1;
     x2>>=up2;
     for(int i=0;i<down2;i++)
         x2=x2*2+1;
-    int ans=o1^o2;
-    if(ans){
-        if(!o1)x1>>=1;
-        else x2>>=1;
-    }
+    //printf("%d %d %d %d\n",up1,down1,up2,down2);
+    //printf("%d %d\n", x1,x2);
+    int ans=0;
     while(x1!=x2){
-        x1>>=1;
-        x2>>=1;
-        ans+=2;
+        if(x1>x2)x1>>=1;
+        else x2>>=1;
+        ans++;
     }
+    //printf("ans=%d\n", ans);
     return ans>z;
 }
 int main(){
