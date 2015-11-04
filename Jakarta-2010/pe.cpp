1ee8bd6f791f7dbd464e5db32df44abbbfb4666d
allenwhale <allenwhale@yahoo.com.tw>
2015-11-04 21:06:29 +0800

    update

--- Jakarta-2010/pe.cpp
+++ Jakarta-2010/pe.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<int> vc[500010];
-int lca[50010][17];
+int lca[50010][18];
 int din[50010], dout[50010];
 int dep;
 int v[50010];
 void dfs(int x,int p){
     din[x]=dep++;
     lca[x][0]=p;
-    for(int i=1;i<17;i++){
+    for(int i=1;i<18;i++){
         lca[x][i]=lca[lca[x][i-1]][i-1];
     }
     for(int i=0;i<(int)vc[x].size();i++){
@@ -24,7 +24,7 @@ bool ancestor(int x, int y){
 int LCA(int x,int y){
     if(ancestor(x,y))return x;
     if(ancestor(y,x))return y;
-    for(int i=16;i>=0;i--){
+    for(int i=17;i>=0;i--){
         if(!ancestor(lca[x][i], y))
             x=lca[x][i];
     }
@@ -72,7 +72,8 @@ int Solve(){
         }else{
             v[a]+=c;
             v[b]+=c;
-            v[lca[l][0]]-=2*c;
+            v[l]-=c;
+            v[lca[l][0]]-=c;
         }
     }
     dfs2(N,N);
