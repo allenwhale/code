cd694bfca6a077c3febc87a6d6a64e8fc4237d64
allenwhale <allenwhale@yahoo.com.tw>
2015-11-21 20:55:47 +0800

    update

--- uva/10074.cpp
+++ uva/10074.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define f first
 #define s second
+
 int mp[110][110];
 int h[110][110];
 int l[110][110];
@@ -11,34 +12,26 @@ int Solve(){
     int N,M;
     scanf("%d%d",&N,&M);
     if(N==0&&M==0)return -1;
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=M;j++){
+    for(int i=1;i<=N;i++)
+        for(int j=1;j<=M;j++)
             scanf("%d",&mp[i][j]);
-        }
-    }
     memset(h,0,sizeof(h));
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=M;j++){
+    for(int i=1;i<=N;i++)
+        for(int j=1;j<=M;j++)
             h[i][j]=(mp[i][j]==0)?h[i-1][j]+1:0;
-        }
-    }
     for(int i=1;i<=N;i++){
         h[i][0]=h[i][M+1]=-1;
         for(int j=1;j<=M;j++)
             l[i][j]=r[i][j]=j;
-        for(int j=1;j<=M;j++){
+        for(int j=1;j<=M;j++)
             while(h[i][j]<=h[i][l[i][j]-1])l[i][j]=l[i][l[i][j]-1];
-        }
-        for(int j=M;j>=1;j--){
+        for(int j=M;j>=1;j--)
             while(h[i][j]<=h[i][r[i][j]+1])r[i][j]=r[i][r[i][j]+1];
-        }
     }
     int ans=0;
-    for(int i=1;i<=N;i++){
-        for(int j=1;j<=M;j++){
+    for(int i=1;i<=N;i++)
+        for(int j=1;j<=M;j++)
             ans=max(ans, h[i][j]*(r[i][j]-l[i][j]+1));
-        }
-    }
     return ans;
 }
 int main(){
