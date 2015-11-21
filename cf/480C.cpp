5099ce8cfe62fee41ddf5a660c17a2ea6022ffef
allenwhale <allenwhale@yahoo.com.tw>
2015-11-22 00:01:06 +0800

    update

---
+++ cf/480C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+int N,A,B,K;
+int dp[5010][5010]={0};
+int sum[5010]={0};
+int get(int a,int b){
+    return (sum[b]-sum[a]+MOD)%MOD;
+}
+void calc(int x){
+    for(int i=1;i<=N;i++)
+        sum[i]=(dp[x][i]+sum[i-1])%MOD;
+}
+int main(){
+    scanf("%d%d%d%d",&N,&A,&B,&K);
+    dp[0][A]=1;
+    calc(0);
+    for(int i=1;i<=K;i++){
+        for(int j=1;j<=N;j++){
+            if(j==B)continue;
+            if(j<B){
+                int end=B-(abs(B-i)>>1)-1;
+                dp[i][j]=(get(0,end)-dp[i-1][j]+MOD)%MOD;
+            }else{
+                int start=B-(abs(B-i)>>1)+1;
+                dp[i][j]=(get(start-1, N)-dp[i-1][j]+MOD)%MOD;
+            }
+        }
+        calc(i);
+    }
+    printf("%d\n", sum[N]);
+    return 0;
+}
